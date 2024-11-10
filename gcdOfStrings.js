eb4d45eab97d4f632941d4a5048954dbee23e096
PhillipDaum <philgdaum@gmail.com>
2024-11-10 14:17:36 -0700

    working

---
+++ gcdOfStrings.js
@@ -0,0 +1,20 @@
+// finds the greatest common divisor between two strings
+
+let a = "ABABAB";
+let b = "ABAB";
+let c = "LEET";
+let d = "CODE";
+
+const gcdOfStrings = (str1, str2) => {
+    // first see if they can even work
+    if (str1 + str2 !== str2 + str1) return '';
+
+    // then, find the gdc of the numbers 
+
+
+
+
+}
+
+// let answer = gcdOfStrings(a,b);
+// console.log(answer)
---
+++ gcdOfStringsBetter.js
@@ -0,0 +1,17 @@
+// finds the greatest common divisor between two strings
+
+let a = "ABABAB";
+let b = "ABAB";
+let c = "LEET";
+let d = "CODE";
+
+const gcdOfStrings = (str1, str2) => {
+    
+
+
+
+
+}
+
+// let answer = gcdOfStrings(a,b);
+// console.log(answer)
